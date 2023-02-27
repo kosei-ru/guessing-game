d930e154c5bee4699e74d46e2ec8aae8f5866a31
kosei-ru <kosei.ru@gmail.com>
2023-02-27 20:44:37 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.start = min;
+        this.end = max;
     }
 
     guess() {
-
+        this.middle = Math.round((this.end + this.start)/2);
+        return this.middle;
     }
 
     lower() {
-
+        this.end = this.middle;
     }
 
     greater() {
-
+        this.start = this.middle;
     }
 }
 
